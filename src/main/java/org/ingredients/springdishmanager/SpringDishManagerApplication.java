d204af5026ae27d87b5bc4684fcb61aef54c3cd4
mayahNeko <mayahibela@gmail.com>
2026-03-29 09:57:13 +0300

    Initialize project

---
+++ src/main/java/org/ingredients/springdishmanager/SpringDishManagerApplication.java
@@ -0,0 +1,13 @@
+package org.ingredients.springdishmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDishManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDishManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ingredients/springdishmanager/SpringDishManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ingredients.springdishmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDishManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
